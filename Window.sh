9da71ee57f892f5be9d41c2385d3985c27ff2b7f
Bitstick <thebitstick@tfwno.gf>
2016-09-15 22:51:27 -0500

    Update Window.sh

--- Window.sh
+++ Window.sh
@@ -3,8 +3,11 @@
 # pictures directory
 XDG_PICTURES_DIR="${XDG_PICTURES_DIR:-$HOME/Pictures}"
 
-# take a screenshot using maim and xdotool
-maim -i $(xdotool getactivewindow) /tmp/sharexin_img.png
+# take a screenshot using gnome-screenshot
+gnome-screenshot -w -f /tmp/sharexin_img.png
+
+# launches ruby script
+ruby ./Picture.rb
 
 # date and time for naming
 date=$(date +%Y-%m-%d)
@@ -12,9 +15,6 @@ time=$(date +%T)
 
 # copies to permanent location
 cp /tmp/sharexin_img.png $XDG_PICTURES_DIR/ShareXin/twitter_window-$date-$time.png
-
-# launches ruby script
-ruby ./Picture.rb
 
 # makes notification
 notify-send "Sent" \
