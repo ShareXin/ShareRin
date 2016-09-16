46e028d789f4f7bbb5f9e89cf892b6ace8f0a3ed
Bitstick <thebitstick@tfwno.gf>
2016-09-15 22:49:31 -0500

    Update Selection.sh

--- Selection.sh
+++ Selection.sh
@@ -3,8 +3,11 @@
 # pictures directory
 XDG_PICTURES_DIR="${XDG_PICTURES_DIR:-$HOME/Pictures}"
 
-# take a screenshot using maim while pausing the screen
-maim /tmp/sharexin_selection.png && eom -f /tmp/sharexin_selection.png & maim -s /tmp/sharexin_img.png --opengl && killall eom
+# take a screenshot using gnome-screen while pausing the screen
+gnome-screenshot -a -f /tmp/sharexin_img.png
+
+# launches ruby script
+ruby ./Picture.rb
 
 # date and time for naming
 date=$(date +%Y-%m-%d)
@@ -12,12 +15,6 @@ time=$(date +%T)
 
 # copies to permanent location
 cp /tmp/sharexin_img.png $XDG_PICTURES_DIR/ShareXin/twitter_clip-$date-$time.png
-
-# launches ruby script
-ruby ./Picture.rb
-
-# cleans up
-killall eom
 
 # makes notification
 notify-send "Sent" \
